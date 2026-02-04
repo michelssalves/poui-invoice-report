a2a1e217e929739e965518583754b65498406c03
michel.alves <ewave.malves@tcp.com.br>
2026-02-04 14:59:22 -0300

    ajustado build

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'poui-projects' title`, () => {
+  it(`should have the 'poui-invoice-report' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('poui-projects');
+    expect(app.title).toEqual('poui-invoice-report');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, poui-projects');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, poui-invoice-report);
   });
 });
