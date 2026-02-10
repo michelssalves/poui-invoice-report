69348caabc9599a35e97b47d100228dd254e1fc6
michel.alves <ewave.malves@tcp.com.br>
2026-02-10 16:46:41 -0300

    Auto-generated commit

---
+++ chunk-T57ICMF6.js
@@ -0,0 +1,16 @@
+// src/environments/environment.ts
+var environment = {
+  production: false,
+  apiUrl: "http://vhwin1065:9023/rest/protheus/v1/poui",
+  useMocks: true,
+  auth: {
+    user: "admin",
+    password: "tcp_tcp"
+  },
+  erpModule: "COM"
+};
+
+export {
+  environment
+};
+//# sourceMappingURL=chunk-T57ICMF6.js.map
